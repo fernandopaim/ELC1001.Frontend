3ebd3b599fb1fba283de57eb4a3ee183db0a4e2b
Mattheus Einloft <mattheuseinloft@gmail.com>
2020-05-14 17:38:04 -0300

    display image of clothes

--- src/telas/looks.js
+++ src/telas/looks.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { SafeAreaView, View, Text, FlatList, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, FlatList, StyleSheet, Image } from 'react-native';
 
 const looks = [
   {
@@ -7,13 +7,13 @@ const looks = [
     name: 'nome do look 1',
     description: 'descrição do look 1',
     clothe_torso: {
-      // Informações da roupa do tronco
+      image_url: 'https://static3.tcdn.com.br/img/img_prod/311840/camisa_umbro_gremio_i_2019_jogador_63175_1_20191202180011.jpg',
     },
     clothe_leg: {
-      // Informações da roupa das pernas
+      image_url: 'https://static3.tcdn.com.br/img/img_prod/311840/calca_umbro_gremio_treino_2019_marinho_56159_1_20191202190710.jpg',
     },
     clothe_feet: {
-      // Informações do calçado
+      image_url: 'https://static.netshoes.com.br/produtos/tenis-umbro-soul-knit-trainer-ii-masculino/26/D21-4975-026/D21-4975-026_zoom1.jpg',
     },
   },
 
@@ -22,13 +22,43 @@ const looks = [
     name: 'nome do look 2',
     description: 'descrição do look 2',
     clothe_torso: {
-      // Informações da roupa do tronco
+      image_url: 'https://images.lojanike.com.br/1024x1024/produto/19791_674359_20190625155435.png',
     },
     clothe_leg: {
-      // Informações da roupa das pernas
+      image_url: 'https://static.lojadointer.com.br/produtos/calca-internacional-nike-dry-academy-kp-masculina/24/HZM-2701-024/HZM-2701-024_zoom1.jpg',
     },
     clothe_feet: {
-      // Informações do calçado
+      image_url: 'https://authenticfeet.vteximg.com.br/arquivos/ids/221345-1000-1000/Tenis-Nike-Odyssey-React-Masculino-Preto.jpg',
+    },
+  },
+
+  {
+    _id: 'id_do_look_3',
+    name: 'nome do look 3',
+    description: 'descrição do look 3',
+    clothe_torso: {
+      image_url: 'https://images.lojanike.com.br/1024x1024/produto/19791_674359_20190625155435.png',
+    },
+    clothe_leg: {
+      image_url: 'https://static.lojadointer.com.br/produtos/calca-internacional-nike-dry-academy-kp-masculina/24/HZM-2701-024/HZM-2701-024_zoom1.jpg',
+    },
+    clothe_feet: {
+      image_url: 'https://static.netshoes.com.br/produtos/tenis-umbro-soul-knit-trainer-ii-masculino/26/D21-4975-026/D21-4975-026_zoom1.jpg',
+    },
+  },
+
+  {
+    _id: 'id_do_look_4',
+    name: 'nome do look 4',
+    description: 'descrição do look 4',
+    clothe_torso: {
+      image_url: 'https://static3.tcdn.com.br/img/img_prod/311840/camisa_umbro_gremio_i_2019_jogador_63175_1_20191202180011.jpg',
+    },
+    clothe_leg: {
+      image_url: 'https://static3.tcdn.com.br/img/img_prod/311840/calca_umbro_gremio_treino_2019_marinho_56159_1_20191202190710.jpg',
+    },
+    clothe_feet: {
+      image_url: 'https://authenticfeet.vteximg.com.br/arquivos/ids/221345-1000-1000/Tenis-Nike-Odyssey-React-Masculino-Preto.jpg',
     },
   },
 ];
@@ -45,10 +75,23 @@ export default class Looks extends Component {
             keyExtractor={look => look._id}
             renderItem={({ item: look }) => (
               <View style={styles.lookContainer}>
-                <Text>{look.name}</Text>
+                <>
+                  <Image
+                    style={styles.torso}
+                    source={{ uri: look.clothe_torso.image_url }}
+                  />
+                  <Image
+                    style={styles.leg}
+                    source={{ uri: look.clothe_leg.image_url }}
+                  />
+                  <Image
+                    style={styles.feet}
+                    source={{ uri: look.clothe_feet.image_url }}
+                  />
+                </>
               </View>
             )}
-            style={styles.lookList}
+            numColumns={2}
           />
         </SafeAreaView>
       </>
@@ -66,15 +109,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#C88E89',
   },
 
-  lookList: {
-    //
-  },
-
   lookContainer: {
     marginTop: 10,
-    marginHorizontal: 10,
+    marginHorizontal: 14,
     backgroundColor: '#fff',
-    padding: 20,
+    padding: 40,
     borderRadius: 10,
   },
+
+  torso: {
+    width: 66,
+    height: 58,
+  },
+
+  leg: {
+    width: 68,
+    height: 86,
+  },
+
+  feet: {
+    width: 70,
+    height: 68,
+  }
 });
