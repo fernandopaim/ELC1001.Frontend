e72c40559d334619ba585ecd4583dea3bf47c4d1
Fernando <fernandosilva1989@gmail.com>
2020-05-24 16:23:39 -0300

    [NEW] Adiciona ajustes de layout

--- src/telas/looks.js
+++ src/telas/looks.js
@@ -1,5 +1,13 @@
-import React, { Component } from 'react';
-import { SafeAreaView, View, Text, FlatList, StyleSheet, Image } from 'react-native';
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const looks = [
   {
@@ -7,13 +15,16 @@ const looks = [
     name: 'nome do look 1',
     description: 'descrição do look 1',
     clothe_torso: {
-      image_url: 'https://static3.tcdn.com.br/img/img_prod/311840/camisa_umbro_gremio_i_2019_jogador_63175_1_20191202180011.jpg',
+      image_url:
+        'https://static3.tcdn.com.br/img/img_prod/311840/camisa_umbro_gremio_i_2019_jogador_63175_1_20191202180011.jpg',
     },
     clothe_leg: {
-      image_url: 'https://static3.tcdn.com.br/img/img_prod/311840/calca_umbro_gremio_treino_2019_marinho_56159_1_20191202190710.jpg',
+      image_url:
+        'https://static3.tcdn.com.br/img/img_prod/311840/calca_umbro_gremio_treino_2019_marinho_56159_1_20191202190710.jpg',
     },
     clothe_feet: {
-      image_url: 'https://static.netshoes.com.br/produtos/tenis-umbro-soul-knit-trainer-ii-masculino/26/D21-4975-026/D21-4975-026_zoom1.jpg',
+      image_url:
+        'https://static.netshoes.com.br/produtos/tenis-umbro-soul-knit-trainer-ii-masculino/26/D21-4975-026/D21-4975-026_zoom1.jpg',
     },
   },
 
@@ -22,13 +33,16 @@ const looks = [
     name: 'nome do look 2',
     description: 'descrição do look 2',
     clothe_torso: {
-      image_url: 'https://images.lojanike.com.br/1024x1024/produto/19791_674359_20190625155435.png',
+      image_url:
+        'https://images.lojanike.com.br/1024x1024/produto/19791_674359_20190625155435.png',
     },
     clothe_leg: {
-      image_url: 'https://static.lojadointer.com.br/produtos/calca-internacional-nike-dry-academy-kp-masculina/24/HZM-2701-024/HZM-2701-024_zoom1.jpg',
+      image_url:
+        'https://static.lojadointer.com.br/produtos/calca-internacional-nike-dry-academy-kp-masculina/24/HZM-2701-024/HZM-2701-024_zoom1.jpg',
     },
     clothe_feet: {
-      image_url: 'https://authenticfeet.vteximg.com.br/arquivos/ids/221345-1000-1000/Tenis-Nike-Odyssey-React-Masculino-Preto.jpg',
+      image_url:
+        'https://authenticfeet.vteximg.com.br/arquivos/ids/221345-1000-1000/Tenis-Nike-Odyssey-React-Masculino-Preto.jpg',
     },
   },
 
@@ -37,13 +51,16 @@ const looks = [
     name: 'nome do look 3',
     description: 'descrição do look 3',
     clothe_torso: {
-      image_url: 'https://images.lojanike.com.br/1024x1024/produto/19791_674359_20190625155435.png',
+      image_url:
+        'https://images.lojanike.com.br/1024x1024/produto/19791_674359_20190625155435.png',
     },
     clothe_leg: {
-      image_url: 'https://static.lojadointer.com.br/produtos/calca-internacional-nike-dry-academy-kp-masculina/24/HZM-2701-024/HZM-2701-024_zoom1.jpg',
+      image_url:
+        'https://static.lojadointer.com.br/produtos/calca-internacional-nike-dry-academy-kp-masculina/24/HZM-2701-024/HZM-2701-024_zoom1.jpg',
     },
     clothe_feet: {
-      image_url: 'https://static.netshoes.com.br/produtos/tenis-umbro-soul-knit-trainer-ii-masculino/26/D21-4975-026/D21-4975-026_zoom1.jpg',
+      image_url:
+        'https://static.netshoes.com.br/produtos/tenis-umbro-soul-knit-trainer-ii-masculino/26/D21-4975-026/D21-4975-026_zoom1.jpg',
     },
   },
 
@@ -52,13 +69,16 @@ const looks = [
     name: 'nome do look 4',
     description: 'descrição do look 4',
     clothe_torso: {
-      image_url: 'https://static3.tcdn.com.br/img/img_prod/311840/camisa_umbro_gremio_i_2019_jogador_63175_1_20191202180011.jpg',
+      image_url:
+        'https://static3.tcdn.com.br/img/img_prod/311840/camisa_umbro_gremio_i_2019_jogador_63175_1_20191202180011.jpg',
     },
     clothe_leg: {
-      image_url: 'https://static3.tcdn.com.br/img/img_prod/311840/calca_umbro_gremio_treino_2019_marinho_56159_1_20191202190710.jpg',
+      image_url:
+        'https://static3.tcdn.com.br/img/img_prod/311840/calca_umbro_gremio_treino_2019_marinho_56159_1_20191202190710.jpg',
     },
     clothe_feet: {
-      image_url: 'https://authenticfeet.vteximg.com.br/arquivos/ids/221345-1000-1000/Tenis-Nike-Odyssey-React-Masculino-Preto.jpg',
+      image_url:
+        'https://authenticfeet.vteximg.com.br/arquivos/ids/221345-1000-1000/Tenis-Nike-Odyssey-React-Masculino-Preto.jpg',
     },
   },
 ];
@@ -66,55 +86,91 @@ const looks = [
 export default class Looks extends Component {
   render() {
     return (
-      <>
-        <Text style={styles.title}>looks</Text>
+      <SafeAreaView style={styles.safeArea}>
+        <LinearGradient
+          colors={['#C58882', '#CFDBDB']}
+          style={styles.gradientContainer}
+          start={{x: 0, y: 0}}
+          end={{x: 0.3, y: 0.7}}>
+          <View style={styles.containerTitle}>
+            <Text style={styles.title}>looks</Text>
+          </View>
 
-        <SafeAreaView style={styles.container}>
-          <FlatList
-            data={looks}
-            keyExtractor={look => look._id}
-            renderItem={({ item: look }) => (
-              <View style={styles.lookContainer}>
-                <>
-                  <Image
-                    style={styles.torso}
-                    source={{ uri: look.clothe_torso.image_url }}
-                  />
-                  <Image
-                    style={styles.leg}
-                    source={{ uri: look.clothe_leg.image_url }}
-                  />
-                  <Image
-                    style={styles.feet}
-                    source={{ uri: look.clothe_feet.image_url }}
-                  />
-                </>
-              </View>
-            )}
-            numColumns={2}
-          />
-        </SafeAreaView>
-      </>
+          <View style={styles.container}>
+            <FlatList
+              data={looks}
+              keyExtractor={look => look._id}
+              numColumns={2}
+              renderItem={({item: look}) => {
+                return (
+                  <View style={styles.gridContainer}>
+                    <View style={styles.lookContainer}>
+                      <Image
+                        style={styles.torso}
+                        source={{uri: look.clothe_torso.image_url}}
+                      />
+                      <Image
+                        style={styles.leg}
+                        source={{uri: look.clothe_leg.image_url}}
+                      />
+                      <Image
+                        style={styles.feet}
+                        source={{uri: look.clothe_feet.image_url}}
+                      />
+                    </View>
+                  </View>
+                );
+              }}
+            />
+          </View>
+        </LinearGradient>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#CFDBDB',
+  },
+
+  gradientContainer: {
+    flex: 1,
+  },
+
+  containerTitle: {
+    alignItems: 'center',
+    height: 30,
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+  },
+
   title: {
     textAlign: 'center',
   },
 
   container: {
     flex: 1,
-    backgroundColor: '#C88E89',
+    padding: 5,
+  },
+
+  gridContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    margin: 5,
+    padding: 5,
+    height: 200,
   },
 
   lookContainer: {
-    marginTop: 10,
-    marginHorizontal: 14,
-    backgroundColor: '#fff',
-    padding: 40,
-    borderRadius: 10,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
   },
 
   torso: {
@@ -130,5 +186,5 @@ const styles = StyleSheet.create({
   feet: {
     width: 70,
     height: 68,
-  }
+  },
 });
